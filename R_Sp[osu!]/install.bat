0864eead69cb0620d6bb983673ec7ee3233d2669
Sendevia <sendevia@163.com>
2018-12-27 00:40:38 +0800

    2018/12/27 - UPDATE
    ·新增全局音效（osu!lazer风格）

--- R_Sp[osu!]/install.bat
+++ R_Sp[osu!]/install.bat
@@ -1,4 +1,4 @@
-@echo off & setlocal enabledelayedexpansion
+@echo off
 :Installed Packs Check
 set "file=packinformation.ini"
 set n=0
@@ -8,8 +8,8 @@ for /f "usebackq tokens=1* delims=: " %%a in ("%file%") do (
 set /a m+=1
 if  !m! LSS !n! set "%%a=%%b"
 )
-echo 你当前安装的包是： %PackName%
-echo 颜色：%Colors%
+echo 你当前安装的包是：%PackName%
+echo 颜色：%Color%
 echo 帧率：%Frame% Fps
 echo 语言：%Language%
 echo 版本：%Release%
