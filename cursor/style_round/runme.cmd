28a2c85e8e3f68dad7ef9663a6eb7a3503a99f70
Sendevia <sendevialol@gmail.com>
2020-08-23 13:51:30 +0800

    [📝] project - 优化脚本

--- cursor/style_round/runme.cmd
+++ cursor/style_round/runme.cmd
@@ -1,7 +1,17 @@
 @echo off
-move /y cursor_0*.png color_blue\cursor.png | move /y cursor_1*.png color_green\cursor.png | move /y cursor_2*.png color_orange\cursor.png | move /y cursor_2*.png color_orange\cursor.png | move /y cursor_3*.png color_pink\cursor.png | move /y cursor_4*.png color_red\cursor.png
-move /y cursor@2x_0*.png color_blue\cursor@2x.png | move /y cursor@2x_1*.png color_green\cursor@2x.png | move /y cursor@2x_2*.png color_orange\cursor@2x.png | move /y cursor@2x_2*.png color_orange\cursor@2x.png | move /y cursor@2x_3*.png color_pink\cursor@2x.png | move /y cursor@2x_4*.png color_red\cursor@2x.png
+move /y cursor_0*.png color_blue\cursor.png
+move /y cursor_1*.png color_green\cursor.png
+move /y cursor_2*.png color_orange\cursor.png
+move /y cursor_2*.png color_orange\cursor.png
+move /y cursor_3*.png color_pink\cursor.png
+move /y cursor_4*.png color_red\cursor.png
+move /y cursor@2x_0*.png color_blue\cursor@2x.png
+move /y cursor@2x_1*.png color_green\cursor@2x.png
+move /y cursor@2x_2*.png color_orange\cursor@2x.png
+move /y cursor@2x_2*.png color_orange\cursor@2x.png
+move /y cursor@2x_3*.png color_pink\cursor@2x.png
+move /y cursor@2x_4*.png color_red\cursor@2x.png
 cls
-echo Finished
+echo Íê³É
 echo. 
 pause
