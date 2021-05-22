fe010e002c38224c9be137c299abdad8fd549d84
Sendevia <sendevialol@gmail.com>
2021-05-22 16:18:25 +0800

    [📝] amix 重命名文件夹

---
+++ tool/main.py
@@ -0,0 +1,17 @@
+import sys
+from PySide6.QtWidgets import *
+from ui import Ui_MainWindow
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
